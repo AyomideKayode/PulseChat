9abbc5bfb23caf8662d0ad032db97ca87b341fc1
Ayomide Kayode <ayomidekay7@gmail.com>
2026-01-14 01:52:41 +0100

    feat (root): Combine client and server dev servers to test for deployment under 1 localhost umbrella as opposed to 2 diff urls. Mimic prod environment. Deploy to Sevalla to test.💫

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,40 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import path from 'path';
+// Route imports
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
+const __dirname = path.resolve();
 
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+// Serve static files from the React frontend app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  // Handle React routing, return all requests to React app
+  app.get('*path', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+  });
+} else {
+  // In development, you'll likely run Vite on a separate port (5173)
+  app.get('/', (req, res) => {
+    res.send(
+      'API is running in development mode. Please run the Vite dev server.'
+    );
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(`💫 PulseChat is running on: ${PORT}`);
+  console.log(`💫 PulseChat is running on: http://localhost:${PORT}`);
 });
